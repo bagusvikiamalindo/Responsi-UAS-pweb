c14657fef7c419a64e8c9fa8762164674d3f458f
Bagus viki amalindo <101860488+bagusvikiamalindo@users.noreply.github.com>
2022-07-21 10:14:54 +0700

    Add files via upload

---
+++ proses.php
@@ -0,0 +1,21 @@
+<?php 
+
+$nama = $_POST['nama'];
+$kodepesanan = $_POST['kodepesanan'];
+$alamat = $_POST['alamat'];
+$Rasa = $_POST['Rasa'];
+$antrian = $_POST['antrian'];
+$komentar = $_POST['komentar'];
+
+echo "<head><title>My Guest Book</title></head>";
+$fp = fopen("data.txt", "a+");
+fputs($fp, "$nama|$kodepesanan|$alamat|$Rasa|$antrian|$komentar\n");
+fclose($fp);
+
+echo "<center><b>Terima kasih Telah Order pesanan</center></b> <br>";
+echo "<a href='index.php'><center>Isi Pesanan anda<center></a><br>";
+echo "<a href ='Penjumlahan.html'><center> Hitung Total harga semua pesanan</center";
+echo "<a href='lihat.php'><center>Lihat Pesanan Anda<center></a><br>";
+
+
+ ?>
